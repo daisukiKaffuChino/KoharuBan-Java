7bf8c78303c7921b17cca9257be4d55b54c224a4
KaffuChino <47238156+daisukiKaffuChino@users.noreply.github.com>
2024-07-20 17:05:39 +0800

    craft detect & bug fix

--- src/main/java/github/daisukiKaffuChino/koharu/listener/PlayerActionListener.java
+++ src/main/java/github/daisukiKaffuChino/koharu/listener/PlayerActionListener.java
@@ -14,8 +14,11 @@ import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.inventory.CraftingInventory;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -23,7 +26,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class PlayerActionListener implements Listener {
     private final JavaPlugin plugin;
@@ -96,10 +98,8 @@ public class PlayerActionListener implements Listener {
                 logUtil.outputLogFile(player, item.getType().name());
             }
 
-            if (item != null && detectIllegalEnchantment(item)) {
-                player.sendMessage("§l§9[KoharuBan] §c非法附魔已移除，你的行为将被记录！");
-                logUtil.outputLogFile(player, item.getType().name() + " - 非法附魔");
-            }
+            if (item != null)
+                detectIllegalEnchantment(player, item);
 
         }
     }
@@ -132,6 +132,22 @@ public class PlayerActionListener implements Listener {
         }
     }
 
+    @EventHandler
+    public void onPrepareItemCraft(PrepareItemCraftEvent event) {
+        CraftingInventory inventory = event.getInventory();
+        ItemStack result = inventory.getResult();
+        if (result != null && bannedItems.containsValue(result.getType())) {
+            inventory.setResult(new ItemStack(Material.AIR));
+            Player player = (Player) event.getView().getPlayer();
+            Inventory playerInventory = player.getInventory();
+            for (ItemStack item : inventory.getMatrix())
+                if (item != null && item.getType() != Material.AIR) playerInventory.addItem(item);//加回去
+            for (int i = 0; i < inventory.getMatrix().length + 1; i++)
+                inventory.setItem(i, new ItemStack(Material.AIR));
+            player.sendMessage(String.format("§l§9[KoharuBan] §c你不能合成%s，材料已返还！", result.getType()));
+        }
+    }
+
     private void checkInventoryAndRemove(Player player) {
         if (opUuid.equals(player.getUniqueId().toString())) return;
         ItemStack[] contents = player.getInventory().getContents();
@@ -143,49 +159,44 @@ public class PlayerActionListener implements Listener {
                 this.logUtil.outputLogFile(player, itemStack.getType().name());
             }
 
-            if (itemStack != null && detectIllegalEnchantment(itemStack)) {
-                player.sendMessage("§l§9[KoharuBan] §c非法附魔已移除，你的行为将被记录！");
-                logUtil.outputLogFile(player, itemStack.getType().name() + " - 非法附魔");
-            }
+            if (itemStack != null)
+                detectIllegalEnchantment(player, itemStack);
+
         }
 
     }
 
     //TODO 检查冲突附魔 例如无限和经验修补
-    private boolean detectIllegalEnchantment(ItemStack item) {
-       /* Map<Enchantment, Integer> enchantments = item.getEnchantments();
-        boolean hasDetected = false;
-        for (Enchantment enchantment : enchantments.keySet()) {
-            if (enchantments.size() > 5 || hasIllegalEnchantment(enchantments, enchantment.getMaxLevel())) {
-                Set<Enchantment> keySet = item.getEnchantments().keySet();
-                keySet.forEach(item::removeEnchantment);
-                hasDetected = true;
-            }
-        }
-        return hasDetected;*/
-
-        boolean hasDetected = false;
+    //TODO 更好的检查过量附魔方法
+    private void detectIllegalEnchantment(Player player, ItemStack item) {
+        //boolean hasDetected = false;
         if (item != null && item.hasItemMeta() && item.getItemMeta().hasEnchants()) {
             Map<Enchantment, Integer> enchantments = item.getEnchantments();
 
-            if (enchantments.size() > 5) {
+            //找到更好的方法之前， Koharu 将不再检查附魔数量
+            //虽然根据知乎高赞回答“铁砧的话，剑最多可以附魔六个，当然每个都是满级魔咒。弓是五个，工具是四个...”，可以把超过 6 个的附魔全部判定为非法，但是假如当 *byd* 玩家们给剑附上了保护时，这一规则就会发生误判
+            /*if (enchantments.size() > 6) {
                 Set<Enchantment> keySet = enchantments.keySet();
                 keySet.forEach(item::removeEnchantment);
                 plugin.getLogger().warning("移除过量附魔，数量：" + enchantments.size());
                 return true;//不需要继续遍历了
-            }
+            }*/
+
             for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
                 Enchantment enchantment = entry.getKey();
                 int level = entry.getValue();
                 if (level > enchantment.getMaxLevel()) {
                     item.removeEnchantment(enchantment); //只移除超最大等级的那个附魔，而非全部
-                    plugin.getLogger().warning(String.format("清除附魔：%s 等级：%s", enchantment.getKey().getKey(), level));
-                    hasDetected = true;
+                    player.sendMessage("§l§9[KoharuBan] §c非法附魔已移除，你的行为将被记录！");
+                    String msg = String.format("清除附魔：%s 等级：%s", enchantment.getKey().getKey(), level);
+                    logUtil.outputLogFile(player, item.getType().name() + msg);
+                    plugin.getLogger().warning(msg);
+                    //hasDetected = true;
                 }
             }
 
         }
-        return hasDetected;
+        //return hasDetected;
     }
 
     private void info(String str) {
